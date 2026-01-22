5e211c0c27bd1adf98c0706baaa4e9a9d027ab4e
while-maybe <178353419+while-maybe@users.noreply.github.com>
2026-01-22 04:10:03 +0000

    feat(main): add clean shutdown functioinality
    - context added to allow clean shutdown from SIGTERM or Interrupt
    - context added to allow in-flight requests to end gracefully with a timeout

--- cmd/blogengine/main.go
+++ cmd/blogengine/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
 	"time"
 
 	"blogengine/internal/content"
@@ -31,7 +35,6 @@ func NewApp(blogTitle string, sourcesDir string, logger *slog.Logger, writer io.
 	if err != nil {
 		return nil, fmt.Errorf("failed to scan data sources: %w", err)
 	}
-
 	repo.LoadLazyMetaFromDisk(files)
 
 	h := handlers.NewBlogHandler(repo, blogTitle, logger)
@@ -41,7 +44,6 @@ func NewApp(blogTitle string, sourcesDir string, logger *slog.Logger, writer io.
 	// static files
 	fs := http.FileServer(http.Dir("static"))
 	mux.Handle("GET /static/", http.StripPrefix("/static/", fs))
-
 	// routes
 	mux.Handle("GET /", h.HandleIndex())
 	mux.Handle("GET /post/", h.HandlePost())
@@ -51,6 +53,7 @@ func NewApp(blogTitle string, sourcesDir string, logger *slog.Logger, writer io.
 		Addr:         ":3000",
 		Handler:      mux,
 		ReadTimeout:  5 * time.Second,
+		IdleTimeout:  30 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 
@@ -61,9 +64,35 @@ func NewApp(blogTitle string, sourcesDir string, logger *slog.Logger, writer io.
 	}, nil
 }
 
-func (a *App) Run() error {
-	a.Logger.Info("server starting", "addr", a.Server.Addr)
-	return a.Server.ListenAndServe()
+func (a *App) Run(ctx context.Context) error {
+
+	srvErrChan := make(chan error, 1)
+
+	go func() {
+		a.Logger.Info("server starting", "addr", a.Server.Addr)
+		if err := a.Server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			srvErrChan <- err
+		}
+	}()
+
+	select {
+	case err := <-srvErrChan:
+		return fmt.Errorf("server startup failed: %w", err)
+	case <-ctx.Done():
+		a.Logger.Info("shutdown signal received")
+	}
+
+	// attempt clean shutdown
+	srvCleanupDuration := 10 * time.Second
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), srvCleanupDuration)
+	defer cancel()
+
+	a.Logger.Info("draining connections...")
+	if err := a.Server.Shutdown(shutdownCtx); err != nil {
+		return fmt.Errorf("graceful shutdown failed: %w", err)
+	}
+	a.Logger.Info("server stopped")
+	return nil
 }
 
 func main() {
@@ -73,13 +102,18 @@ func main() {
 	logHandler := slog.NewTextHandler(stderr, &slog.HandlerOptions{Level: slog.LevelInfo})
 	logger := slog.New(logHandler).With("app", blogTitle)
 
+	rootCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	// initialise
 	app, err := NewApp(blogTitle, "./sources", logger, stderr)
 	if err != nil {
 		logger.Error("server initialise", "err", err)
 		os.Exit(1)
 	}
 
-	if err := app.Run(); err != nil {
+	// run the app with context
+	if err := app.Run(rootCtx); err != nil {
 		logger.Error("server crashed", "err", err)
 		os.Exit(1)
 	}
